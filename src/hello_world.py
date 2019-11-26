7d38b803174b98f46701f06a4b23b222d8ccecad
KadaliKiranKumar <kadalikirank@esntechnologies.co.in>
2019-11-26 17:13:51 +0530

    Revert "initial commit in task_item_1 branch"

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,8 +1,4 @@
 print("hellow world")
 print("this is kiran")
 print("First time modified")
-print("This is commiting from task_item_1 branch")
-print("This is the second commiting from task_item_1 branch")
-print("This is the third commiting from task_item_1 branch")
-
-
+print("This is commiting from develop branch")
